028d8dab3127ae8ba31495971f8f2267a10e77cb
Leo Torres <leo.htor@gmail.com>
2015-03-31 18:05:46 -0700

    "input type changed to button, stopping page reload."

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # audio_test
+# an application that provides a dashbord for musicians on the move with #a place to quickly tune-up and record random moments of inspiration.
--- audio.js
+++ audio.js
@@ -1,5 +1,7 @@
 function addListeners() {
 
+
+
 var currentlyPlaying = null;
 
 var stopSound = function () {
@@ -12,6 +14,7 @@ var toggleSound = function (sound) {
     currentlyPlaying.stop();
     currentlyPlaying = null;
   } else {
+    console.log(sound);
     stopSound();
     sound.play();
     currentlyPlaying = sound;
@@ -43,34 +46,16 @@ $("#highE" ).click(function() {
 });
 
 
-$("#stopPlay").click(stopSound);
+// $("#stopPlay").click(stopSound);
 
 
-}
-
-var voice = new Wad({source: 'mic' }); // At this point, your browser will ask for permission to access your microphone.
-var tuner = new Wad.Poly();
-tuner.add(voice);
-
-// voice.play(); // You must give your browser permission to access your microphone before calling play().
-
-// tuner.updatePitch(); // The tuner is now calculating the pitch and note name of its input 60 times per second. These values are stored in tuner.pitch and tuner.noteName.
-
-// var logPitch = function(){
-//     console.log(tuner.pitch, tuner.noteName);
-//     requestAnimationFrame(logPitch);
-// };
-// logPitch();
-// // If you sing into your microphone, your pitch will be logged to the console in real time.
-
-// tuner.stopUpdatingPitch(); // Stop calculating the pitch if you don't need to know it anymore.
 
 
-// Creation of Notes for WADOscillator to be created on 'play();'
+
 
 
 // attempt to create one function as a template for each new WAD that I need.
-var createWads = function(frequency) {
+var createSounds = function(frequency) {
   new Wad ({
     source: "triangle",
     pitch: frequency,
@@ -79,96 +64,94 @@ var createWads = function(frequency) {
       decay: 0,
       sustain: 1,
       hold: 120,
-      release: 1
+      release: 0.2
     }
   });
 };
 
-var E2 = createWads(82.41);
-var A2 = createWads(110);
-var D3 = createWads(146.83);
-var G3 = createWads(196);
+
+var E2 = createSounds(82.41);
+var A2 = createSounds(110);
+var D3 = createSounds(146.83);
+var G3 = createSounds(196);
+var B3 = createSounds(246.94);
+var E4 = createSounds(329.63);
+
+
 
 // var E2 = new Wad({
 //   source: "triangle",
 //   pitch: 82.41,
 //   env: {
-//     attack: 1,
-//     decay: 1,
+//     attack: 0,
+//     decay: 0,
 //     sustain: 1,
 //     hold: 120,
-//     release: 1
+//     release: 0.2
 //   }
 // });
 
-var A2 = new Wad({
-  source: "triangle",
-  pitch: 110,
-  env: {
-    attack: 0,
-    decay: 1,
-    sustain: 1,
-    hold: 120,
-    release: 0.5
-  }
-});
+// var A2 = new Wad({
+//   source: "triangle",
+//   pitch: 110,
+//   env: {
+//     attack: 0,
+//     decay: 0,
+//     sustain: 1,
+//     hold: 120,
+//     release: 0.2
+//   }
+// });
 
-var D3 = new Wad({
-  source: "triangle",
-  pitch: 146.83,
-  env: {
-    attack: 0,
-    decay: 1,
-    sustain: 1,
-    hold: 120,
-    release: 0.5
-  }
-});
+// var D3 = new Wad({
+//   source: "triangle",
+//   pitch: 146.83,
+//   env: {
+//     attack: 0,
+//     decay: 0,
+//     sustain: 1,
+//     hold: 120,
+//     release: 0.2
+//   }
+// });
 
-var G3 = new Wad({
-  source: "triangle",
-  pitch: 196,
-  env: {
-    attack: 0,
-    decay: 1,
-    sustain: 1,
-    hold: 120,
-    release: 0.5
-  }
-});
+// var G3 = new Wad({
+//   source: "triangle",
+//   pitch: 196,
+//   env: {
+//     attack: 0,
+//     decay: 0,
+//     sustain: 1,
+//     hold: 120,
+//     release: 0.2
+//   }
+// });
 
-var B3 = new Wad({
-  source: "triangle",
-  pitch: 246.94,
-  env: {
-    attack: 0,
-    decay: 1,
-    sustain: 1,
-    hold: 120,
-    release: 0.5
-  }
-});
+// var B3 = new Wad({
+//   source: "triangle",
+//   pitch: 246.94,
+//   env: {
+//     attack: 0,
+//     decay: 0,
+//     sustain: 1,
+//     hold: 120,
+//     release: 0.2
+//   }
+// });
 
-var E4 = new Wad({
-  source: "triangle",
-  pitch: 329.63,
-  env: {
-    attack: 0,
-    decay: 1,
-    sustain: 1,
-    hold: 120,
-    release: 0.5
-  }
-});
+// var E4 = new Wad({
+//   source: "triangle",
+//   pitch: 329.63,
+//   env: {
+//     attack: 0,
+//     decay: 0,
+//     sustain: 1,
+//     hold: 120,
+//     release: 0.2
+//   }
+// });
 
-
-
-
-
-window.onload = addListeners; 
-
-
-
-
+}
+window.onload = addListeners();
 
 
