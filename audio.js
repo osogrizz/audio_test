5c6409d9e7ad8a7d05c362dbbec28d9c3dfe565b
Leo Torres <leo.htor@gmail.com>
2015-03-31 20:18:34 -0700

    restyled record buttons

--- audio.js
+++ audio.js
@@ -151,7 +151,38 @@ var E4 = new Wad({
   }
 });
 
-var rec = new Recorder(source, [ config]);
+function __log(e, data) {
+    log.innerHTML += "\n" + e + " " + (data || '');
+  }
+  var audio_context;
+  var recorder;
+  function startUserMedia(stream) {
+    var input = audio_context.createMediaStreamSource(stream);
+    __log('Media stream created.');
+    // Uncomment if you want the audio to feedback directly
+    //input.connect(audio_context.destination);
+    //__log('Input connected to audio context destination.');
+    
+    recorder = new Recorder(input);
+    __log('Recorder initialised.');
+  }
+  function startRecording(button) {
+    recorder && recorder.record();
+    button.disabled = true;
+    button.nextElementSibling.disabled = false;
+    __log('Recording...');
+  }
+  function stopRecording(button) {
+    recorder && recorder.stop();
+    button.disabled = true;
+    button.previousElementSibling.disabled = false;
+    __log('Stopped recording.');
+    
+    // create WAV download link using audio data blob
+    createDownloadLink();
+    
+    recorder.clear();
+  }
 
 
 
